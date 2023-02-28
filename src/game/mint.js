d301e530204cd620f3e877e26cfa3cdb4c9cebc4
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-01 00:00:45 +0800

    basic style for mint page

--- src/game/mint.js
+++ src/game/mint.js
@@ -1,5 +1,7 @@
 import React from "react";
 import preview from "../images/nft-preview.gif"
+import './mint.css';
+import { Button } from '@mui/material';
 
 const Mint = ({contract}) => {
 
@@ -14,12 +16,18 @@ const Mint = ({contract}) => {
     return (
         <React.Fragment>
             <div className="mint-container">
-                <p>Mint a Charachter</p>
-                <div>
-                <img src={preview} alt="preview" width="100%" />
+                <h1>Mint a Character</h1>
+                <div className="mint-image">
+                    <img src={preview} alt="preview" width="50%" />
                 </div>
-                <div>
-                    <button onClick={simpleMint}>Mint</button>
+                <div className="mint-button">
+                    <Button 
+                        onClick={simpleMint}
+                        variant="contained"
+                        color="primary"
+                    >
+                        Mint
+                    </Button>
                 </div>
             </div>
         </React.Fragment>
