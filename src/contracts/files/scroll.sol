8609b77f03bc08d7ec478ae9fdfdb94da4591777
Kyle Koshiyama <43145199+kkoshiya@users.noreply.github.com>
2023-03-03 00:32:38 -0800

    updated contract

--- src/contracts/files/scroll.sol
+++ src/contracts/files/scroll.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
 contract NFTArena is ERC1155 {
     uint256 public constant PLAYER = 0;
@@ -13,7 +15,8 @@ contract NFTArena is ERC1155 {
 
     string private baseURI;
     uint256 public playerCount;
-    bool public arenaOpen;
+    uint256 public swordCount;
+    bool public arenaOpen = true;
     Arena public arena = Arena(true, 0, payable(msg.sender));
 
     mapping(uint => Player) public players;
@@ -21,18 +24,25 @@ contract NFTArena is ERC1155 {
     mapping(uint => Train) public trainings;
     mapping(uint => uint) public URIs;
     mapping(address => uint256[]) public addressToPlayers;
+    mapping(address => uint256[]) public addressToSwords;
     mapping(address => bool) public owners;
+    mapping(uint => address) public owner;
+    mapping(uint => Sword) public swords;
+
 
     enum Status {
         idle,
         questing,
-        training
+        training,
+        arena
     }
 
     struct Player {
         uint256 hp;
         uint256 attack;
         Status status;
+        uint256 wins;
+        bool item;
     }
 
     struct Quest {
@@ -49,6 +59,11 @@ contract NFTArena is ERC1155 {
         address payable hostAddress;
     }
 
+    struct Sword {
+        bool available;
+        address owner;
+    }
+
     constructor()
         ERC1155(
             "https://bafybeihfvy2hmcnvpax6anx3tgx53qie4nj32eqtuehsu2g5c5hx3ukxc4.ipfs.nftstorage.link/"
@@ -62,15 +77,27 @@ contract NFTArena is ERC1155 {
         _;
     }
 
+    modifier ownerOf(uint _tokenId) {
+        require(owner[_tokenId] == msg.sender);
+        _;
+    }
+
+    modifier unequpied(uint _tokenId) {
+        require(players[_tokenId].item == false);
+        _;
+    }
+
     function _mintPlayer() external {
         playerCount++;
         _mint(msg.sender, PLAYER, 1, "");
+        _mint(msg.sender, GOLD, 5, "");
 
-        URIs[playerCount] = random() % 598 + 1;
-        players[playerCount] = Player(10, 1, Status.idle);
+        URIs[playerCount] = random(playerCount) % 598 + 1;
+        players[playerCount] = Player(200, 2, Status.idle, 0, false);
         quests[playerCount] = Quest(0);
         trainings[playerCount] = Train(0);
         addressToPlayers[msg.sender].push(playerCount);
+        owner[playerCount] = msg.sender;
     }
 
     function uri(uint256 _id) public view override returns (string memory) {
@@ -81,21 +108,26 @@ contract NFTArena is ERC1155 {
         return s;
     }
 
+
     function getPlayers(address _address) public view returns (uint256[] memory) {
         return addressToPlayers[_address];
     }
 
-    function setIdle(uint256 _tokenId) internal {
+    function getSwords(address _address) public view returns (uint256[] memory) {
+        return addressToSwords[_address];
+    }
+
+    function setIdle(uint256 _tokenId) internal ownerOf(_tokenId) {
         players[_tokenId].status = Status.idle;
     }
 
-    function startQuest(uint256 _tokenId) external isIdle(_tokenId) {
+    function startQuest(uint256 _tokenId) external isIdle(_tokenId) ownerOf(_tokenId){
         players[_tokenId].status = Status.questing;
         Quest storage quest = quests[_tokenId];
-        quest.endTime = block.timestamp + 120;
+        quest.endTime = block.timestamp + 1;
     }
 
-    function endQuest(uint256 _tokenId) external {
+    function endQuest(uint256 _tokenId) external ownerOf(_tokenId) {
         require(
             block.timestamp >= quests[_tokenId].endTime,
             "It's not time to finish quest"
@@ -105,12 +137,12 @@ contract NFTArena is ERC1155 {
         quests[_tokenId].endTime = 0;
     }
 
-    function startTraining(uint256 _tokenId) external isIdle(_tokenId) {
+    function startTraining(uint256 _tokenId) external isIdle(_tokenId) ownerOf(_tokenId) {
         players[_tokenId].status = Status.training;
         trainings[_tokenId].startTime = block.timestamp;
     }
 
-    function endTraining(uint256 _tokenId) external {
+    function endTraining(uint256 _tokenId) external ownerOf(_tokenId) {
         require(
             block.timestamp >= trainings[_tokenId].startTime + 120,
             "it's too early to pull out"
@@ -119,50 +151,132 @@ contract NFTArena is ERC1155 {
         players[_tokenId].attack++; //we can make this logic more complex later
     }
 
-    function enterArena(uint256 _tokenId) external isIdle(_tokenId) {
+    function getBlocktime() external view  returns (uint256) {
+        return (block.timestamp);
+    }
+
+    // function enterArena(uint256 _tokenId) external isIdle(_tokenId) {
+    //     require(arena.open, "arena is closed");
+    //     require(balanceOf(msg.sender, 1) >= 1, "not enough gold");
+    //     _safeTransferFrom(msg.sender, address(this), 1, 1, "0x0");
+    //     arena.open = false;
+    // }
+
+    // function fightArena(uint256 _tokenId) external isIdle(_tokenId) {
+    //     require(!arena.open, "arena is empty");
+    //     require(balanceOf(msg.sender, 1) >= 1, "not enough gold");
+    //     uint256 winner = simulateFight(arena.hostId, _tokenId);
+    //     if (winner == _tokenId) {
+    //         safeTransferFrom(address(this), msg.sender,  1, 1, "0x0");
+    //     } else {
+    //         safeTransferFrom(address(this), arena.hostAddress,  1, 1, "0x0");
+    //         safeTransferFrom(msg.sender, arena.hostAddress,  1, 1, "0x0");
+    //     }
+    //     arena.open = false;
+    // }
+
+    // function simulateFight(uint256 _hostId, uint256 _challengerId) internal view returns(uint256) {
+    //     Player storage host = players[_hostId];
+    //     Player storage challenger = players[_challengerId];
+    //     uint hostHp = host.hp;
+    //     uint challengerHp = challenger.hp;
+    //     while(hostHp > 0 && challengerHp > 0) {
+    //         challengerHp - host.attack * (random() % 2);
+    //         if (challengerHp <= 0) {
+    //             break;
+    //         }
+    //         hostHp - challenger.attack * (random() % 2);
+    //     }
+    //     return _challengerId; 
+    // }
+
+    function enterArena(uint256 _tokenId) external isIdle(_tokenId) ownerOf(_tokenId) {
         require(arena.open, "arena is closed");
         require(balanceOf(msg.sender, 1) >= 1, "not enough gold");
         safeTransferFrom(msg.sender, address(this), 1, 1, "0x0");
+        players[_tokenId].status = Status.arena;
         arena.open = false;
+        arena.hostId = _tokenId;
+        arena.hostAddress = payable(msg.sender);
     }
 
-    function fightArena(uint256 _tokenId) external isIdle(_tokenId) {
+    function fightArena(uint256 _tokenId) external isIdle(_tokenId) ownerOf(_tokenId) {
         require(!arena.open, "arena is empty");
         require(balanceOf(msg.sender, 1) >= 1, "not enough gold");
         uint256 winner = simulateFight(arena.hostId, _tokenId);
         if (winner == _tokenId) {
-            safeTransferFrom(address(this), msg.sender,  1, 1, "0x0");
+            players[_tokenId].wins++;
+            _mint(msg.sender, GOLD, 1, "");
         } else {
-            safeTransferFrom(address(this), arena.hostAddress,  1, 1, "0x0");
-            safeTransferFrom(msg.sender, arena.hostAddress,  1, 1, "0x0");
+            _mint(msg.sender, GOLD, 1, "");
+            players[arena.hostId].wins++;
+            safeTransferFrom(msg.sender, arena.hostAddress, 1, 1, "0x0");
         }
-        arena.open = false;
+        arena.open = true;
+        players[arena.hostId].status = Status.idle;
     }
 
-    function simulateFight(uint256 _hostId, uint256 _challengerId) internal view returns(uint256) {
+    function simulateFight(uint256 _hostId, uint256 _challengerId)
+        internal
+        view
+        returns (uint256)
+    {
         Player storage host = players[_hostId];
         Player storage challenger = players[_challengerId];
-        uint hostHp = host.hp;
-        uint challengerHp = challenger.hp;
-        while(hostHp > 0 && challengerHp > 0) {
-            challengerHp - host.attack * (random() % 2);
-            if (challengerHp <= 0) {
-                break;
-            }
-            hostHp - challenger.attack * (random() % 2);
+        uint hostPoints = (host.hp * (random(_hostId) % 5)) - (challenger.attack  * (random(_challengerId) % 4));
+        uint challengerPoints = (challenger.hp * (random(_challengerId) % 5)) - (host.attack  * (random(_hostId) % 5));
+        if ( hostPoints >= challengerPoints ) {
+            return _hostId;
+        } else {
+            return _challengerId;
         }
-        return _challengerId; 
     }
 
-    function random() internal view returns(uint256) {
-        return uint256(keccak256(abi.encodePacked(block.timestamp + block.difficulty + playerCount)));
+    function openArena() public {
+        arena.open = true;
+    }
+    
+
+    function random(uint _tokenId) internal view returns(uint256) {
+        return uint256(keccak256(abi.encodePacked(block.timestamp + block.difficulty + playerCount + _tokenId)));
     }
 
-    function craftSword(uint256 _tokenId) external isIdle(_tokenId) {
-        require(balanceOf(msg.sender, 1) >= 10, "you don't have enough gold");
-        safeTransferFrom(msg.sender, address(this), 1, 10, "0x0");
-        _mint(msg.sender, SWORD, 3, "");
+    function craftSword(uint256 _tokenId) external isIdle(_tokenId) ownerOf(_tokenId) unequpied(_tokenId) {
+        require(balanceOf(msg.sender, 1) >= 5, "you don't have enough gold");
+        safeTransferFrom(msg.sender, address(this), 1, 3, "0x0");
+        swordCount++;
+        swords[swordCount].available = true;
+        addressToSwords[msg.sender].push(swordCount);
+        _mint(msg.sender, SWORD, 1, "");
     }
 
+    function equipSword(uint256 _tokenId, uint256 _swordId) external isIdle(_tokenId) ownerOf(_tokenId) {
+        require(swords[_swordId].available, "this sword is not avliable right now");
+        players[_tokenId].item = true;
+        players[_tokenId].attack += 5;
+        swords[_swordId].available = false;
+        swords[_swordId].owner = msg.sender;
+    }
+
+    function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes memory
+    ) public virtual returns (bytes4) {
+        return this.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] memory,
+        uint256[] memory,
+        bytes memory
+    ) public virtual returns (bytes4) {
+        return this.onERC1155BatchReceived.selector;
+    }
+    
 
 }
