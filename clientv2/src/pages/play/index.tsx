be9ac7a8608bf9f0b1516674566825a274c98e48
Alexandre Carvalheira <axxcar@gmail.com>
2023-03-22 22:33:16 -0300

    fix mint component

--- clientv2/src/pages/play/index.tsx
+++ clientv2/src/pages/play/index.tsx
@@ -56,12 +56,13 @@ export default function Play() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        className="relative w-fit mb-auto min-h-fit flex flex-col sm:flex-row items-center justify-center mx-auto  h-[78vh]"
+        className="relative w-fit mb-auto min-h-fit flex flex-col items-center justify-center mx-auto  h-[78vh]"
       >
         {" "}
-        <div>
+        {/* <div>
           <span className="inline-flex h-6 w-6 animate-spin rounded-full border-4 border-dotted border-purple-800"></span>
-        </div>
+        </div> */}
+        <Mint />
       </motion.div>
     );
   }
