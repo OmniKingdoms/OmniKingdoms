9701872867d1ff560e5b89f346df96b34f379fef
Kyle Koshiyama <43145199+kkoshiya@users.noreply.github.com>
2023-03-25 22:03:59 -0700

    yes

---
+++ deployment/hardhat.config.js
@@ -0,0 +1,77 @@
+const { ethers } = require('ethers');
+const dotenv = require('dotenv');
+dotenv.config();
+
+/* global ethers task */
+require('@nomiclabs/hardhat-waffle')
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-diamond-abi");
+
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task('accounts', 'Prints the list of accounts', async () => {
+  const accounts = await ethers.getSigners()
+
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+})
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const keys = process.env.WALLET;
+const scrollUrl = 'https://alpha-rpc.scroll.io/l2';
+
+module.exports = {
+  solidity: '0.8.17',
+  hardhat: {
+    allowUnlimitedContractSize: true,
+  },
+  diamondAbi: {
+    // (required) The name of your Diamond ABI
+    name: "DIAMOND-1-HARDHAT",
+    strict: false
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.MUMBAI_KEY,
+      scroll: process.env.ETHERSCAN
+    },
+    customChains: [
+      {
+        network: "scroll",
+        chainId: 534353,
+        urls: {
+          apiURL: "https://blockscout.scroll.io/api",
+          browserURL: "https://blockscout.scroll.io"
+        }
+      }
+    ]
+  },
+  networks: {
+    scroll: {
+      url: scrollUrl,
+      accounts: [keys]
+    }, 
+    mumbai: {
+      allowUnlimitedContractSize: true,
+      gas: 2100000,
+      gasPrice: 8000000000,
+      gasLimit: 50000000000000,
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [keys]
+    }
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
+}
---
+++ deployment/scripts/deploy.js
@@ -0,0 +1,113 @@
+/* global ethers */
+/* eslint prefer-const: "off" */
+const hre = require('hardhat');
+const { getSelectors, FacetCutAction } = require('./libraries/diamond.js')
+console.log('this is env', process.env.WALLET)
+
+async function deployDiamond () {
+  const accounts = await ethers.getSigners()
+  const contractOwner = accounts[0]
+
+  // Deploy DiamondInit
+  // DiamondInit provides a function that is called when the diamond is upgraded or deployed to initialize state variables
+  // Read about how the diamondCut function works in the EIP2535 Diamonds standard
+  const DiamondInit = await ethers.getContractFactory('DiamondInit')
+  const diamondInit = await DiamondInit.deploy()
+  await diamondInit.deployed()
+  console.log('DiamondInit deployed:', diamondInit.address)
+
+  // Deploy facets and set the `facetCuts` variable
+  console.log('')
+  console.log('Deploying facets')
+  const FacetNames = [
+    'DiamondCutFacet',
+    'DiamondLoupeFacet',
+    'OwnershipFacet',
+    'PlayerFacet',
+    'QuestFacet',
+    'CraftFacet',
+    'TrainFacet',
+    'EquipFacet'
+  ]
+  // The `facetCuts` variable is the FacetCut[] that contains the functions to add during diamond deployment
+  const facetCuts = []
+  for (const FacetName of FacetNames) {
+    const Facet = await ethers.getContractFactory(FacetName)
+    const facet = await Facet.deploy()
+    await facet.deployed()
+    console.log(`${FacetName} deployed: ${facet.address}`)
+    facetCuts.push({
+      facetAddress: facet.address,
+      action: FacetCutAction.Add,
+      functionSelectors: getSelectors(facet)
+    })
+
+    await verifyContract(facet, FacetName);
+  }
+
+  // Creating a function call
+  // This call gets executed during deployment and can also be executed in upgrades
+  // It is executed with delegatecall on the DiamondInit address.
+  let functionCall = diamondInit.interface.encodeFunctionData('init')
+  console.log('function call', functionCall)
+
+  // Setting arguments that will be used in the diamond constructor
+  const diamondArgs = {
+    owner: contractOwner.address,
+    init: diamondInit.address,
+    initCalldata: functionCall
+  }
+  // deploy Diamond
+  const Diamond = await ethers.getContractFactory('Diamond')
+  const diamond = await Diamond.deploy(facetCuts, diamondArgs)
+  await diamond.deployed()
+
+  
+
+  console.log()
+  console.log('Diamond deployed:', diamond.address)
+
+  await verifyContract(diamond, 'Diamond', [facetCuts, diamondArgs]);
+ 
+  // returning the address of the diamond
+  return diamond.address
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+if (require.main === module) {
+  deployDiamond()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error)
+      process.exit(1)
+    })
+}
+
+exports.deployDiamond = deployDiamond
+
+async function verifyContract (diamond, FacetName, constructorArguments = []) {
+  const liveNetworks = ['mainnet', 'goerli', 'mumbai', 'scroll'];
+  if (!liveNetworks.includes(hre.network.name)) {
+    return; // Don't verify on test networks
+  }
+
+  try {
+    console.log("Waiting for 10 blocks to be mined...");
+    await diamond.deployTransaction.wait(10);
+    console.log("Running verification");
+    await hre.run("verify:verify", {
+      address: diamond.address,
+      contract: `contracts/facets/${FacetName}.sol:${FacetName}`,
+      network: hardhatArguments.network,
+      arguments: constructorArguments ? constructorArguments : [],
+    });
+  } catch (e) {
+    console.log("Verification failed: ", JSON.stringify(e, null, 2));
+  }
+
+  // hre.run('verify:verify', {
+  //   address: diamond.address,
+  //   constructorArguments
+  // })
+}
