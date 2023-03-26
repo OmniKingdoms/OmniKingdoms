fc5289dfa5057eca6a3879502b50d213c288d93f
Alexandre Carvalheira <axxcar@gmail.com>
2023-03-25 21:09:01 -0300

    scroll alpha network

--- clientv2/src/context/wagmiProvider.tsx
+++ clientv2/src/context/wagmiProvider.tsx
@@ -1,11 +1,30 @@
 import { Web3Modal } from "@web3modal/react";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { configureChains, createClient, WagmiConfig, Chain } from "wagmi";
 import { polygonMumbai, mainnet, polygon } from "wagmi/chains";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { publicProvider } from "wagmi/providers/public";
 import "@rainbow-me/rainbowkit/styles.css";
 
-const chains = [polygonMumbai, polygon, mainnet];
+export const scrollAlpha = {
+  id: 534353,
+  name: "ScrollAlpha",
+  network: "ScrollAlpha",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ethereum",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    public: { http: ["https://alpha-rpc.scroll.io/l2"] },
+    default: { http: ["https://alpha-rpc.scroll.io/l2"] },
+  },
+  blockExplorers: {
+    etherscan: { name: "blockscout", url: "https://blockscout.scroll.io/" },
+    default: { name: "blockscout", url: "https://blockscout.scroll.io/" },
+  },
+} as const satisfies Chain;
+
+const chains = [scrollAlpha, polygonMumbai, polygon, mainnet];
 
 const { connectors } = getDefaultWallets({
   appName: "Scroll Kingdoms",
