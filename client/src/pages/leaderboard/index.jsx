b9ac515dc3cba2dad136d50d87a9cfa950949a7f
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-04-12 15:32:51 +0800

    feat: install MUI, add LB template, show data

--- client/src/pages/leaderboard/index.jsx
+++ client/src/pages/leaderboard/index.jsx
@@ -1,6 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import LeaderboardTable from "../../components/leaderboardTable";
 
 const Leaderboard = () => {
+    const [players, setPlayers] = useState({});
 
     useEffect(() => {
         try {
@@ -10,6 +12,21 @@ const Leaderboard = () => {
             })
             .then((parsedData) => {
                 console.log(`parsedData`, parsedData);
+                
+                // modify image url before setting data - because infura-ipfs.io images not loading
+                const modifiedData = {
+                    ...parsedData,
+                    players: parsedData.players.map((player) => {
+                        const modifiedPlayer = { ...player };
+                        modifiedPlayer.image = modifiedPlayer.image.replace("https://infura-ipfs.io", "https://cloudflare-ipfs.com");
+                        return modifiedPlayer;
+                    })
+                };
+                
+                console.log(`modifiedData`, modifiedData);
+                console.log(`modifiedData.players`, modifiedData.players)
+                setPlayers(modifiedData.players);
+
             })
             .catch((error) => {
                 console.log(error);
@@ -20,7 +37,16 @@ const Leaderboard = () => {
     }, [])
 
     return (
-        <div>Leaderboard</div>
+        <div>
+            <h1 className="text-white">Leaderboard</h1>
+            <div className="mx-[2rem]">
+                {players && 
+                    <LeaderboardTable 
+                        players={players}
+                    />
+                }
+            </div>
+        </div>
     )
 }
 
