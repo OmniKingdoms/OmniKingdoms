f131ca67398518cd80c5aeda90ccd965e436d77a
developerkunal <dawar.kunal703@gmail.com>
2023-04-14 01:58:43 +0530

    Added New Update

--- backend-api/models/Player.js
+++ backend-api/models/Player.js
@@ -1,15 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const playerSchema = new mongoose.Schema({
   id: { type: Number, required: true, unique: true },
-  strength: { type: Number, required: true, index: true },
-  magic: { type: Number, required: true },
-  health: { type: Number, required: true },
-  status: { type: String, required: true },
+  owner:{type:String,required:true},
+  name: { type: String, required: true },
+  level: { type: Number, required: true, default: 1 },
+  strength: { type: Number, required: true, index: true, default: 1 },
+  magic: { type: Number, required: true, default: 1 },
+  health: { type: Number, required: true, default: 10 },
+  status: { type: Number, required: true, default: 0 },
   image: { type: String, required: true },
-  xp: { type: Number, required: true, index: true }
-});
+  xp: { type: Number, required: true, index: true, default: 0 },
+  wins: { type: Number, required: true, default: 0 },
+  losses: { type: Number, required: true, default: 0 },
+  Magicwins: { type: Number, required: true, default: 0 },
+  Magiclosses: { type: Number, required: true, default: 0 },
+  Mainwins: { type: Number, required: true, default: 0 },
+  Mainlosses: { type: Number, required: true, default: 0 },
+  mana:{type:Number,required:true,default:1},
+ 
+},{ timestamps: true });
 
-const Player = mongoose.model('Player', playerSchema);
+const Player = mongoose.model("Player", playerSchema);
 
 module.exports = Player;
