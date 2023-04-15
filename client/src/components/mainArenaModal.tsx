bec7ea9dd6f0982c955926454e881dcbf0e2fdd5
developerkunal <dawar.kunal703@gmail.com>
2023-04-15 10:54:15 +0530

    Reverted Change on Mainarena

--- client/src/components/mainArenaModal.tsx
+++ client/src/components/mainArenaModal.tsx
@@ -22,9 +22,7 @@ export default function MainArenaModal() {
     setArena(openArena?.[0] as any);
     return openArena?.[1].toNumber();
   }
-  async function openArena(){
-     await diamond?.openArenas();
-  }
+
   async function getHost() {
     const hostId = await getArena();
     const player = await diamond?.getPlayer(hostId as any);
@@ -45,6 +43,7 @@ export default function MainArenaModal() {
       getHost();
     }
   }, [player?.status, arena]);
+
   async function handleEnterArena() {
     const provider = new ethers.providers.Web3Provider(window.ethereum as any);
     try {
@@ -85,6 +84,7 @@ export default function MainArenaModal() {
       }
     }
   }
+
   async function handleFightArena() {
     const provider = new ethers.providers.Web3Provider(window.ethereum as any);
     // Get signer
