57379c717c12c79d24929705d66b2dc5e3bd18b0
Gaurang Torvekar <gaurangtorvekar@gmail.com>
2023-06-04 11:44:01 +0100

    Added infra for ERC721 tokens

--- deployment/contracts/upgradeInitializers/DiamondInit.sol
+++ deployment/contracts/upgradeInitializers/DiamondInit.sol
@@ -1,18 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/******************************************************************************\
-* Author: Nick Mudge <nick@perfectabstractions.com> (https://twitter.com/mudgen)
-* EIP-2535 Diamonds: https://eips.ethereum.org/EIPS/eip-2535
-*
-* Implementation of a diamond.
-/******************************************************************************/
+/**
+ * \
+ * Author: Nick Mudge <nick@perfectabstractions.com> (https://twitter.com/mudgen)
+ * EIP-2535 Diamonds: https://eips.ethereum.org/EIPS/eip-2535
+ *
+ * Implementation of a diamond.
+ * /*****************************************************************************
+ */
 
-import { LibDiamond } from "../libraries/LibDiamond.sol";
-import { IDiamondLoupe } from "../interfaces/IDiamondLoupe.sol";
-import { IDiamondCut } from "../interfaces/IDiamondCut.sol";
-import { IERC173 } from "../interfaces/IERC173.sol";
-import { IERC165 } from "../interfaces/IERC165.sol";
+import {LibDiamond} from "../libraries/LibDiamond.sol";
+import {IDiamondLoupe} from "../interfaces/IDiamondLoupe.sol";
+import {IDiamondCut} from "../interfaces/IDiamondCut.sol";
+import {IERC173} from "../interfaces/IERC173.sol";
+import {IERC165} from "../interfaces/IERC165.sol";
+import "../ERC721Storage.sol";
 
 // It is expected that this contract is customized if you want to deploy your diamond
 // with data from a deployment script. Use the init function to initialize state variables
@@ -21,11 +24,13 @@ import { IERC165 } from "../interfaces/IERC165.sol";
 // Adding parameters to the `init` or other functions you add here can make a single deployed
 // DiamondInit contract reusable accross upgrades, and can be used for multiple diamonds.
 
-contract DiamondInit {    
-
-    // You can add parameters to this function in order to pass in 
+contract DiamondInit {
+    // You can add parameters to this function in order to pass in
     // data to set your own state variables
     function init() external {
+        //TODO - Change the name based on discussions
+        ERC1155Storage.layout()._name = "OmniKingdoms Players";
+        ERC1155Storage.layout()._symbol = "OKP";
         // adding ERC165 data
         LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
         ds.supportedInterfaces[type(IERC165).interfaceId] = true;
@@ -33,11 +38,11 @@ contract DiamondInit {
         ds.supportedInterfaces[type(IDiamondLoupe).interfaceId] = true;
         ds.supportedInterfaces[type(IERC173).interfaceId] = true;
 
-        // add your own state variables 
-        // EIP-2535 specifies that the `diamondCut` function takes two optional 
+        // add your own state variables
+        // EIP-2535 specifies that the `diamondCut` function takes two optional
         // arguments: address _init and bytes calldata _calldata
         // These arguments are used to execute an arbitrary function using delegatecall
         // in order to set state variables in the diamond during deployment or an upgrade
-        // More info here: https://eips.ethereum.org/EIPS/eip-2535#diamond-interface 
+        // More info here: https://eips.ethereum.org/EIPS/eip-2535#diamond-interface
     }
 }
